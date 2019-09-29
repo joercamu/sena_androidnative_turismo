08eb591430d299bb2dfcabbdae0ef22feba7f0bc
Jonathan Ernesto Cardenas Murillo <jcardenasm@celsia.com>
2019-09-29 16:52:05 -0500

    quitar clases de Sitio no utilizadas

--- app/src/main/java/appjc/joercamu/aplicacionturismonativo/TurismoListActivity.java
+++ app/src/main/java/appjc/joercamu/aplicacionturismonativo/TurismoListActivity.java
@@ -35,7 +35,7 @@ public class TurismoListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_operator_list);
+        setContentView(R.layout.activity_turismo_list);
 
         addOperatorBtn = findViewById(R.id.btnAddOperator);
         getOperatorListBtn = findViewById(R.id.btnGetOperatorList);
